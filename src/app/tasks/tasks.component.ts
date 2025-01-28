a2ef26d60e4586b9d288f6b8672ff65804ceecba
gentlecode1 <jimenezdefrutos@gmail.com>
2025-01-28 23:26:48 +0100

    feat: implement search

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { debounceTime, filter, mergeWith, Subject, switchMap } from 'rxjs';
+import { MatInputModule } from '@angular/material/input';
+import { BehaviorSubject, combineLatest, filter, map, mergeWith, Subject, switchMap } from 'rxjs';
 import { AddTaskDialogComponent } from './add-task-dialog/add-task-dialog.component';
 import { Task } from './task.type';
 import { TaskComponent } from './task/task.component';
@@ -13,17 +15,38 @@ import { TasksService } from './tasks.service';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, TaskComponent, MatButtonModule, MatIconModule],
-  templateUrl: './tasks.component.html',
+  imports: [CommonModule, FormsModule, MatDialogModule, TaskComponent, MatButtonModule, MatIconModule, MatInputModule],
+  template: `
+  <div class="tasks-container">
+    <div class="button-container">
+        <button mat-icon-button aria-label="Example icon button with a vertical three dot icon" (click)="createTask()">
+            <mat-icon fontIcon="add"></mat-icon>
+        </button>
+    </div>
+    <input matInput type="text" [(ngModel)]="searchValue">
+    @for (task of filteredTasks$ | async; track task.id){
+    <app-task [task]="task" (isCompletedEventEmitter)="editTask($event)"></app-task>
+    }
+</div>`,
   styleUrl: './tasks.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TasksComponent {
   private readonly tasksService = inject(TasksService);
   private readonly matDialog = inject(MatDialog);
-  private readonly onCreteTask = new Subject<void>();
+  private readonly onCreateTask = new Subject<void>();
   private readonly onEditTask = new Subject<Task>();
-  private readonly afterCreateTask$ = this.onCreteTask.asObservable().pipe(
+
+  private searchValueSubject = new BehaviorSubject<string>('');
+  protected searchValue$ = this.searchValueSubject.asObservable();
+  protected get searchValue(): string {
+    return this.searchValueSubject.value;
+  }
+
+  protected set searchValue(value: string) {
+    this.searchValueSubject.next(value);
+  }
+  private readonly afterCreateTask$ = this.onCreateTask.asObservable().pipe(
     switchMap(() => this.matDialog.open<AddTaskDialogComponent, undefined, Task>(AddTaskDialogComponent).afterClosed().pipe(filter(task => !!task))),
     switchMap((task) => this.tasksService.postTask(task)),
     switchMap(() => this.tasksService.getTasks()),
@@ -32,7 +55,16 @@ export class TasksComponent {
     switchMap((task) => this.tasksService.editTask(task)),
     switchMap(() => this.tasksService.getTasks()),
   );
-  readonly tasks$ = this.tasksService.getTasks().pipe(mergeWith(this.afterCreateTask$, this.afterEditTask$));
-  createTask = () => this.onCreteTask.next();
+  protected readonly tasks$ = this.tasksService.getTasks().pipe(mergeWith(this.afterCreateTask$, this.afterEditTask$));
+  protected filteredTasks$ = combineLatest([this.tasks$, this.searchValue$]).pipe(
+    map(([tasks, searchValue]) => this.filterTasks(tasks, searchValue)),
+  );
+
+  private filterTasks(tasks: Task[], searchValue: string): Task[] {
+    const lowerSearch = searchValue.toLowerCase();
+    return tasks.filter(task =>
+      task.title.toLowerCase().includes(lowerSearch) ?? false);
+  }
+  protected createTask = () => this.onCreateTask.next();
   editTask = (task: Task) => this.onEditTask.next(task);
 }
